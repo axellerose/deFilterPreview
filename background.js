a4273bf49e67e2a9833bd195ade7f32b3d0256e2
alex <iamalex.dev@gmail.com>
2022-06-22 15:08:13 +0200

    fetch added

--- background.js
+++ background.js
@@ -1,20 +1,3 @@
-chrome.alarms.create({
-  periodInMinutes: 1 / 60
-})
-
-chrome.alarms.onAlarm.addListener((alarm) => {
-  chrome.storage.local.get('timer', (res) => {
-    let time = res.timer ?? 0
-    chrome.storage.local.set({
-      timer: time + 1
-    })
-    chrome.action.setBadgeText({
-      text: `${time + 1}`
-    })
-  })
-
-})
-
 chrome.browserAction.onClicked.addListener(function (activeTab) {
   chrome.tabs.executeScript(null, {
     target: { tabId: tabId },
--- contentScripts.js
+++ contentScripts.js
@@ -1,14 +1,23 @@
 console.log('content runs')
 
+const retrieveRowCount = () => {
+  fetch('https://cloud.coms.opap.gr/filter-activity-json', { mode: 'no-cors' })
+    .then(response => response.json())
+    .then(data => console.log(data))
+}
 const selectFilterParameters = () => {
+  const allInFilter = document.querySelectorAll('.expression-text > .ft-grouping > *')
 
-
+  allInFilter.forEach(element => {
+    console.log(element.textContent + ' ')
+  })
 }
 
 const filterButton = document.createElement('button')
 filterButton.id = 'filterButton'
-filterButton.onclick = selectFilterParameters
+filterButton.onclick = retrieveRowCount
 filterButton.innerHTML = 'Count Rows'
-'position: absolute; z-index: 999; height: 50px; width: 60px; bottom: 50px; border-radius: 5px; left: calc(50% - 30px); background-color: #3a67aa; color: #fff;'
+filterButton.style.cssText = 'position: absolute; z-index: 999; height: 50px; width: 80px; top: 60px; border-radius: 5px; left: calc(50% - 40px); background-color: #3a67aa; color: #fff;'
 
 document.body.appendChild(filterButton)
+
