90621f024beafc5c05fc45d9f4652e2b0ef7cfe8
alex <iamalex.dev@gmail.com>
2022-06-22 12:04:22 +0200

    new button

--- contentScripts.js
+++ contentScripts.js
@@ -1,27 +1,14 @@
 console.log('content runs')
 
-var function1 = function () {
+const selectFilterParameters = () => {
 
-  console.log('function runs')
 
-  let filterElements = document.querySelectorAll('.ft-grouping')
-  let filtersArray = []
-
-  for (let filterRow of filterElements) {
-    console.log(filterRow)
-  }
-
-  chrome.storage.local.set({
-    filterRows: filtersArray
-  })
-
-  console.log('check end')
 }
 
-var button1 = document.createElement('button')
-button1.id = 'button1'
-button1.onclick = function1
-button1.innerHTML = 'Button 1'
-button1.style.cssText = 'position: absolute; z-index: 999; height: 50px; width: 75px; top:70px; left: 250px; background-color: #e73373; color: #fff;'
+const filterButton = document.createElement('button')
+filterButton.id = 'filterButton'
+filterButton.onclick = selectFilterParameters
+filterButton.innerHTML = 'Count Rows'
+'position: absolute; z-index: 999; height: 50px; width: 60px; bottom: 50px; border-radius: 5px; left: calc(50% - 30px); background-color: #3a67aa; color: #fff;'
 
-document.body.appendChild(button1)
+document.body.appendChild(filterButton)
