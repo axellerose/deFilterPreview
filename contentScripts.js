889c103d3fcf20ac5ff11b87ea9400150b7eb25a
alex <iamalex.dev@gmail.com>
2022-06-22 11:30:09 +0200

    btn bug

--- contentScripts.js
+++ contentScripts.js
@@ -24,11 +24,4 @@ button1.onclick = function1
 button1.innerHTML = 'Button 1'
 button1.style.cssText = 'position: absolute; z-index: 999; height: 50px; width: 75px; top:70px; left: 250px; background-color: #e73373; color: #fff;'
 
-// let showButtonListener = setInterval(() => {
-//   document.querySelectorAll('.expressioneer-filter') && document.body.appendChild(button1);
-// }, 500);
-
-// let hideButtonListener = setInterval(() => {
-//   document.querySelectorAll('.expressioneer-filter') ?? document.body.removeChild(button1);
-// })
 document.body.appendChild(button1)
