0c4198d13b9d3b93e37f19770c23621c7ca11e11
alex <iamalex.dev@gmail.com>
2022-06-27 14:02:36 +0200

    button styles

--- contentScripts.js
+++ contentScripts.js
@@ -11,9 +11,9 @@ const retrieveRowCount = (filter) => {
   fetch(`https://timer-extension.ngrok.io/rows?filter=${filter}`)
     .then(response => response.json())
     .then(data => {
-      const { RowCount } = data;
+      const { RowCount } = data
       console.log(RowCount)
-      /// show me somewhere
+      document.querySelector('#filterInput').value = RowCount
     })
     .catch(error => console.error(JSON.stringify(error)))
 }
@@ -33,15 +33,48 @@ const sendFilter = () => {
 
 const filterButton = document.createElement('button')
 filterButton.id = 'filterButton'
-filterButton.onclick = function (){sendFilter(), retrieveRowCount()}
+filterButton.onclick = function () { sendFilter(), retrieveRowCount() }
 filterButton.innerHTML = 'Run Filter'
-filterButton.style.cssText = 'position: absolute; z-index: 999; font-size: 12px; height: 50px; width: 90px; top: 60px; border-radius: 5px; left: calc(50% - 40px); background-color: #3a67aa; color: #fff;'
+filterButton.style.cssText = 'font-size: 12px; height: 26px; margin-right: 10px; '
+filterButton.classList.add('btn', 'btn-primary', 'btn-large')
 
 const filterInput = document.createElement('input')
 filterInput.id = 'filterInput'
 filterInput.type = 'text'
-filterInput.style.cssText = 'position: absolute; z-index: 999; font-size: 12px; height: 30px; width: 160px; top: 70px; border-radius: 5px; left: calc(50% + 60px); background-color: #fff; color: #000;'
+filterInput.style.cssText = 'font-size: 12px; color: #000;border: 1px solid #D6D6D6;padding: 1px 3px;border-radius: 6px; margin-top: 3px;'
 filterInput.placeholder = 'Filter key'
 
-document.body.appendChild(filterButton)
-document.body.appendChild(filterInput)
+const filterOutput = document.createElement('h2')
+filterOutput.id = 'filterOutput'
+filterOutput.style.cssText = 'color: #e73e73;'
+filterOutput.textContent = ''
+
+const filterContainer = document.createElement('div')
+filterContainer.style.cssText = 'display: flex; justify-content: center; z-index: 999; position: relative; width: 30%; margin-left: 35%;'
+
+filterContainer.id = 'filterContainer'
+filterContainer.appendChild(filterButton)
+filterContainer.appendChild(filterOutput)
+filterContainer.appendChild(filterInput)
+
+
+
+setInterval(() => {
+  const headerContainer = document.querySelector('.op-head')
+
+  const editFilterPage = document.querySelector('.filter-text-heading')
+  const filterOverviewPage = document.querySelector('.ft-filter-preview-source')
+  const headerInEdit = document.querySelector('.expressions-wrap')
+
+  const container = document.querySelector('#filterContainer')
+
+  if ((editFilterPage || filterOverviewPage) && container) {
+
+  } else if (filterOverviewPage) {
+    headerContainer.appendChild(filterContainer)
+  } else if (editFilterPage) {
+    headerInEdit.appendChild(filterContainer)
+  } else {
+    filterContainer.remove()
+  }
+}, 1000)
--- server/routes/rows.js
+++ server/routes/rows.js
@@ -1,26 +1,30 @@
-var express = require('express');
-var router = express.Router();
-const axios = require('axios');
+var express = require('express')
+var router = express.Router()
+const axios = require('axios')
 
 /* GET users listing. */
 router.get('/', async (req, res, next) => {
   try {
-    const { filter } = req.query;
+    const { filter } = req.query
     let resp = await axios.get("https://cloud.coms.opap.gr/custom-filter", {
       params: {
         filter,
       }
-    });
-    const rows = resp.data;
-    console.log(resp.data);
-    res.status(200).json(rows);
+    })
+    const rows = resp.data
+    console.log(resp.data)
+    res.status(200).json(rows)
+
+    chrome.storage.local.set('filterData', {
+      filterData: resp.data
+    })
   } catch (e) {
     console.error("Filter eroror", e)
-    res.status(500).send();
+    res.status(500).send()
   }
 
 
 
-});
+})
 
-module.exports = router;
+module.exports = router
